db7eb738d3ac58e39203b9fc236c881a1f4df64e
Abhilash Das <abhilashmyworld@gmail.com>
2016-10-08 12:19:53 +0530

    Add files via upload

---
+++ app/src/main/java/in/zollet/abhilash/maps/API/LocationData.java
@@ -0,0 +1,30 @@
+package in.zollet.abhilash.maps.API;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Abhilash on 7/31/2016.
+ */
+public class LocationData {
+    private List<Route> routes = new ArrayList<Route>();
+    private String status;
+
+    public List<Route> getRoutes() {
+        return routes;
+    }
+
+    public void setRoutes(List<Route> routes) {
+        this.routes = routes;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
